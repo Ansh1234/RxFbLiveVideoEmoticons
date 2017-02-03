2a5c224b0a8ccbe8a41a9d23eb533583f0da7a17
anshul <anshul.jain@verse.in>
2017-02-03 15:32:20 +0530

    First Commit

---
+++ app/src/main/java/com/rxjava/demos/LiveEmoticon.java
@@ -0,0 +1,42 @@
+package com.rxjava.demos;
+
+/**
+ * Created by anshul on 3/2/17.
+ */
+
+public class LiveEmoticon {
+
+  private Emoticons emoticons;
+  private int xCordinate;
+  private int yCordinate;
+
+  public LiveEmoticon(Emoticons emoticons, int xCordinate, int yCordinate) {
+    this.emoticons = emoticons;
+    this.xCordinate = xCordinate;
+    this.yCordinate = yCordinate;
+  }
+
+  public Emoticons getEmoticons() {
+    return emoticons;
+  }
+
+  public void setEmoticons(Emoticons emoticons) {
+    this.emoticons = emoticons;
+  }
+
+  public int getxCordinate() {
+    return xCordinate;
+  }
+
+  public void setxCordinate(int xCordinate) {
+    this.xCordinate = xCordinate;
+  }
+
+  public int getyCordinate() {
+    return yCordinate;
+  }
+
+  public void setyCordinate(int yCordinate) {
+    this.yCordinate = yCordinate;
+  }
+}
